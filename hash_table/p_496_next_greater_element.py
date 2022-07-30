524c949557a2fd2227a9ed8dc1e9140134d93ed9
apandastider <45123127+apandastider@users.noreply.github.com>
2022-07-29 21:17:23 -0400

    Create p_496_next_greater_element.py

---
+++ hash_table/p_496_next_greater_element.py
@@ -0,0 +1,32 @@
+'''
+The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+
+You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+
+For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. If there is no next greater element, then the answer for this query is -1.
+
+Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+
+'''
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        hash_nums2 = {}
+        
+        for idx, num in enumerate(nums2):
+            hash_nums2[num] = idx
+            
+        result = []
+        flag = 0
+        for num in nums1:
+            flag = 0
+            j = hash_nums2[num]
+            while j < len(nums2):
+                if nums2[j] > num:
+                    flag = 1
+                    result.append(nums2[j])
+                    break
+                j += 1
+            if flag == 0:
+                result.append(-1)
+        return result
