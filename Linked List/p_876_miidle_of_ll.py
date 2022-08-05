481e9bd47f1981ff52b5b063063d9ca4eb3e38b6
apandastider <45123127+apandastider@users.noreply.github.com>
2022-08-05 13:12:24 -0400

    Create p_876_miidle_of_ll.py

---
+++ Linked List/p_876_miidle_of_ll.py
@@ -0,0 +1,37 @@
+'''
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        list_of_heads = []
+        
+        
+        while head!= None:
+            
+            list_of_heads.append(head.val)
+            head = head.next
+            
+        if len(list_of_heads) == 1:
+            return ListNode(list_of_heads[0])
+        
+        if len(list_of_heads) == 2:
+            return ListNode(list_of_heads[1])
+        
+        mid_val_idx = int(len(list_of_heads)/2)
+        q = ListNode(val = list_of_heads[-1])
+        out = None
+        for j in range(len(list_of_heads)-2, mid_val_idx-1, -1):
+            out = ListNode(val = list_of_heads[j], next = q)
+            q = out
+        return out    
+        
