958debdeae490a14c2fa2c0fd4f1578ede2e5060
apandastider <45123127+apandastider@users.noreply.github.com>
2022-08-05 23:38:30 -0400

    Create p_200_no_of_islands.py

---
+++ Graphs/p_200_no_of_islands.py
@@ -0,0 +1,28 @@
+'''
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+'''
+
+## I took help from discussions and Google to understand the problem! :( 
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    count += 1
+        return count
+    def dfs(self, grid, i, j):
+        if i < 0 or j < 0 or i > len(grid) - 1 or j > len(grid[0]) - 1 or grid[i][j] != '1':
+            return
+        grid[i][j] = "#" ## visited
+        
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i, j-1)
+        self.dfs(grid, i, j+1)
