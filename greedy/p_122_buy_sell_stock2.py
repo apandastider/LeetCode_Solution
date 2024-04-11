625550de205c58e6fcb34fa7f0ddcbd601638ad1
apandastider <45123127+apandastider@users.noreply.github.com>
2024-04-10 23:00:41 -0400

    Create p_122_buy_sell_stock2.py

---
+++ greedy/p_122_buy_sell_stock2.py
@@ -0,0 +1,42 @@
+'''
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+Find and return the maximum profit you can achieve.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Total profit is 4 + 3 = 7.
+'''
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        l = 0
+        r = 1
+
+        profit = 0
+        tot_profit = 0
+
+        while r < len(prices):
+            if prices[r] > prices[l]:
+                curr_profit = prices[r] - prices[l]
+                profit += curr_profit
+            l += 1
+            r += 1
+            # else:
+            #     l = r
+            # if prices[r] > prices[r+1]:
+            #     l = r + 1
+            #     tot_profit += profit
+            #     profit = 0
+            # r += 1
+        return profit
+        
