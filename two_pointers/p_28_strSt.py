0f939a0f6edf1160d3f17bc730365e564f34676e
apandastider <45123127+apandastider@users.noreply.github.com>
2024-08-22 12:22:34 -0400

    Update p_28_strSt.py

--- two_pointers/p_28_strSt.py
+++ two_pointers/p_28_strSt.py
@@ -28,4 +28,23 @@ class Solution:
                     return i
         if flag == 0:
             return -1
+% new solution
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+
+        if needle == haystack:
+            return 0 
+
+        i = 0
+
+        while i < len(haystack) and i+len(needle) <= len(haystack):
+
+            if needle == haystack[i:i+len(needle)]:
+
+                return i
+            i += 1
+        return - 1
+
+
         
